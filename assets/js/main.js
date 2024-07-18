96ccbb997c57f41a0715509682fc01f3e7e4a020
Clementine Digout <clementine.digout@yahoo.fr>
2024-07-18 11:12:00 -0400

    change letter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,46 @@
 //Méthode pour vérifier si l'utilisateur trace dans les lettre : Le canvas a une opacité de 0, donc si l'utilisateur se trouve sur un pixel opaque, il a perdu
 
-
+var myFont = new FontFace('Quicksand','url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap")');
 //Selectionner le canvas
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 //Sélectionner le bouton clear
-var btnClear = document.querySelector('.btn-clear');
+const btnClear = document.querySelector('.btn-clear');
 //Sélectionner la modale et les messages
-var modale = document.querySelector('.modale');
-var rules = document.querySelector('.rules');
-var lost = document.querySelector('.lost');
-var win = document.querySelector('.win');
-var btnClose = document.querySelectorAll('.btn-close');
+const modale = document.querySelector('.modale');
+const lost = document.querySelector('.lost');
+const win = document.querySelector('.win');
+const btnClose = document.querySelectorAll('.btn-close');
+const levelHtml = document.querySelector('.number_level');
 
 //Définir le contexte
-var context = canvas.getContext('2d');
+const context = canvas.getContext('2d', {willReadFrequently: true});
 //Définir la variable mousedown (quand le clic est pressé) à faux
-var mousedown = false;
+let mousedown = false;
 //Définir les points de vases de X et Y à rien
-var oldXAxis = null;
-var oldYAXis = null;
+let oldXAxis = null;
+let oldYAxis = null;
 //Définir la variable pixel, égal à rien
-var pixels = null;
+let pixels = null;
 //Définir la variable letterPixel, qui contient le nombre de pixel dans la lettre
-var letterPixels = null;
+let letterPixels = null;
+//
+let completePourcent = 0;
+let leftCanvas = 0;
+let topCanvas = 0;
+let level = 1;
 
-function showRules() {
-    modale.classList.remove('hidden');
-    rules.classList.remove('hidden');
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
 }
+
+window.addEventListener('load', () => {
+    let boundingRect = canvas.getBoundingClientRect();
+    leftCanvas = boundingRect.left;
+    topCanvas = boundingRect.top;
+})
 
 //Fermeture de la modale
 btnClose.forEach(btn => {
@@ -35,21 +48,21 @@ btnClose.forEach(btn => {
         modale.classList.add('hidden');
         win.classList.add('hidden');
         lost.classList.add('hidden');
-        rules.classList.add('hidden');
     })
 })
 
 //Fonction qui définit la largeur de la ligne, l'arrondi et la couleur, la police, et appelle la fonction drawLetter pour dessiner une lettre, puis met à jour la valeur de pixel et la valeur de letterPixels 
 function setupCanvas() {
-    context.lineWidth = 20;
+    context.lineWidth = 25;
     context.lineCap = 'round';
-    context.strokeStyle = 'black';
-    context.font = 'bold 300px Quicksand'
-    context.fillStyle = 'red';
+    context.strokeStyle = 'rgb(255,0,0)';
+    context.font = 'bold 300px Quicksand';
+    context.fillStyle = 'green';
     context.BaseLine = 'middle';
-    drawLetter('R');
-    pixels = context.getImageData (0, 0, canvas.width, canvas.height);
-    letterPixels = getPixelAmount(255, 0, 0);
+    drawLetter(letters[getRandomNumber(26)]);
+    pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+    letterPixels = getPixelAmount(0,255,0);
+    completePourcent = 0;
 }
 
 //Fonction qui prend en paramètre une lettre. D'abord centre la lettre en prenant en compte la largeur et la hauteur de la lettre, puis remplit la lettre avec la méthode fillText qui prend en paramètre la lettre, le centre X et le centre Y
@@ -63,17 +76,31 @@ function showError(){
     mousedown = false;
     modale.classList.remove('hidden');
     lost.classList.remove('hidden');
+    level = 1;
+    updateLevel();
     clearCanvas();
     setupCanvas();
 }
 
 function pixelCompare() {
-    if (letterPixels / getPixelAmount(0, 0, 0) < 0.148) {
+    completePourcent = Math.round((getPixelAmount(255,0,0) / letterPixels) *100);
+    if (completePourcent > 50) {
         modale.classList.remove('hidden');
         win.classList.remove('hidden');
+        clearCanvas();
+        setupCanvas();
+        changeLevel();
     }
 }
 
+function changeLevel() {
+    level++;
+    updateLevel();
+}
+
+function updateLevel() {
+    levelHtml.textContent = level;
+}
 
 //Fonction qui change la valeur de l'état du clic (Quand le clic est activé, il le reconnait par l'évènement)
 function onMouseDown(ev) {
@@ -90,8 +117,8 @@ function onMouseUp(ev) {
 
 //Fonction qui s'applique par l'évènement quand la souris bouge : définit une variable qui entre la position sur les axes X et Y de l'utilisateur puis si le clic est activé, appelle la fonction paint
 function onMouseMove(ev) {
-    var xAxis = ev.clientX;
-    var yAxis = ev.clientY;
+    var xAxis = ev.clientX - Math.round(leftCanvas);
+    var yAxis = ev.clientY - Math.round(topCanvas);
     if (mousedown) {
         paint(xAxis, yAxis);
     }
@@ -104,16 +131,19 @@ function paint(xAxis, yAxis) {
     if (color.a === 0) {
         showError();
     } else {
-    context.beginPath();
+        context.beginPath();
+        context.strokeStyle = 'rgb(255,0,0)'
+        context.lineTo(xAxis, yAxis);
+        context.stroke();
+        context.closePath();
+        oldXAxis = xAxis;
+        oldYAxis = yAxis;
+
     if (oldXAxis > 0 && oldYAxis > 0) {
         context.moveTo(oldXAxis, oldYAxis);
     }
 }
-    context.lineTo(xAxis, yAxis);
-    context.stroke();
-    context.closePath();
-    oldXAxis = xAxis;
-    oldYAxis = yAxis;
+
 }
 
 //Fonction qui prend en compte la valeur des pixels (Un peu compliquée, prise sur google)
@@ -128,7 +158,7 @@ function getPixelColor(xAxis, yAxis) {
 }
 
 //Fonction qui calcule le nombre de pixels qui seront colorés par l'utilisateur (Un peu compliquée, prise sur google)
-function getPixelAmount(r, g, b) {
+function getPixelAmount(r,g,b) {
     var pixels = context.getImageData(0, 0, canvas.width, canvas.height);
     var all = pixels.data.length;
     var amount = 0;
@@ -150,9 +180,6 @@ function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-//Montrer les r`gles du jeu au chargement de la page
-showRules();
-
 //Appel de la fonction de mise en place du Canvas
 setupCanvas();
 
@@ -160,6 +187,10 @@ setupCanvas();
 canvas.addEventListener('mousedown', onMouseDown, false);
 canvas.addEventListener('mouseup', onMouseUp, false);
 canvas.addEventListener('mousemove', onMouseMove, false);
+
+canvas.addEventListener('touchstart', onMouseDown, false);
+canvas.addEventListener('touchend', onMouseUp, false);
+canvas.addEventListener('touchmove', onMouseMove, false);
 
 //Ecoute l'évènement au clic du bouton Clear
 btnClear.addEventListener('click', () => {
